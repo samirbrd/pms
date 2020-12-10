f45c1737e45c6ae16120744f85c52aed732eeba2
samirbrd <39365800+samirbrd@users.noreply.github.com>
2020-12-10 15:42:52 +0000

    Update pmsstreamlit.py

--- pmsstreamlit.py
+++ pmsstreamlit.py
@@ -9,7 +9,7 @@ Created on Thu Dec 10 15:07:54 2020
 import streamlit as st
 import pandas as pd
 
-data = pd.read_csv('C:/Users/Administrator/Google Drive/Autotrading/return analysis.csv')
+data = pd.read_csv('return analysis.csv')
 st.title("Portfolio Management Scheme Analysis (WIP Check back on December 19, 2020)")
 st.write("By Meet Shah")
 st.dataframe(data)
